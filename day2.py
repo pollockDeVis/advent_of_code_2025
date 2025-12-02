f818c33d2d20518b515a97ec000bef66f19dbda2
PBiswas <p.biswas@tudelft.nl>
2025-12-02 18:23:39 +0100

    second part of the second puzzle

--- day2.py
+++ day2.py
@@ -9,6 +9,7 @@ def symmetric_check(digit):
     # Check if length is even
     if length % 2 != 0:
         return False
+        # repetition_check(digit)
     
     half_length = length // 2
 
@@ -22,6 +23,54 @@ def symmetric_check(digit):
     return True
 
 
+def repetition_check(digit):
+    match = False
+    digits = [int(d) for d in str(digit)]
+
+    # Count the occurrences of each unique digit
+    digit_count = {}
+    for d in digits:
+        if d in digit_count:
+            digit_count[d] += 1
+        else:
+            digit_count[d] = 1
+
+
+    # print(digit_count)
+
+    # First case where digit count dict has only one key
+    if len(digit_count) == 1:
+        # print(f"All digits are the same: {digits[0]}")
+        # Check if the value is more than 1
+        if digit_count[digits[0]] > 1:  
+            return True
+    
+    # Now check for repeats of 2 or more digits but it should be less than half the length (half - 1)
+    length = len(digits)
+    half_length = length // 2
+
+    # Start with window size of 2 until half_length - 1
+    for window_size in range(2, half_length):
+        init_window = digits[0:window_size]
+        if length % window_size == 0:
+            match = False
+            for i in range(0, length, window_size):
+                current_window = digits[i:i+window_size]
+                if current_window == init_window:
+                    match = True
+                    continue
+                else:
+                    match = False
+                    break
+    return match
+
+            
+            
+
+
+  
+
+
 def id_range_parser(id_range):
     start, end = map(int, id_range.split('-'))
     return list(range(start, end + 1))
@@ -31,12 +80,12 @@ def id_range_parser(id_range):
 if __name__ == "__main__":
 
 
-    digit = 23232323
+    # digit = 23232323
 
 
-    id_range = "24-78"
-    id_list = id_range_parser(id_range)
-    print(f"ID List: {id_list}")
+    # id_range = "24-78"
+    # id_list = id_range_parser(id_range)
+    # print(f"ID List: {id_list}")
 
 
     # Read range from input_day2.txt #Example ranges 959516-995437,389276443-389465477,683-1336, ...
@@ -53,12 +102,19 @@ if __name__ == "__main__":
     for id in id_ranges:
         
         if symmetric_check(id):
-            print(f"ID {id} has symmetric halves.")
+            # print(f"ID {id} has symmetric halves.")
             # Convert the id to an integer and add to the list
             id = int(id)
             
             # Append to symmetric_ids
             symmetric_ids.append(id)
+        else:
+            if repetition_check(id):
+                # print(f"ID {id} has repeating digits.")
+                id = int(id)
+            
+                # Append to symmetric_ids
+                symmetric_ids.append(id)
             
             
 
