e063e007553d0df45e3d51fc7eae709210b4a0bd
PBiswas <p.biswas@tudelft.nl>
2025-12-07 14:49:12 +0100

    second part of day 6

--- day6.py
+++ day6.py
@@ -9,47 +9,72 @@ if __name__ == "__main__":
     # Read the txt file which has lines strings separated by newlines
     with open("input_day6.txt", "r") as file:
         lines = file.readlines()
-        lines = [line.strip() for line in lines]
+        # lines = [line.strip() for line in lines] # 
+
+    # print("Data lines before processing:", lines)
+    # Length of data lines
+
+    print("Length of data lines:", len(lines), len(lines[0]))
+
+    print("Length of LAST data lines:", len(lines), len(lines[-1]))
+
+    combined_number_list = []
+    operation_flag = False
+    operator = ""
+    results = []
+    for colidx in range(len(lines[-1]), 0, -1):
+        # print("Column index:", colidx)
+        numbers = []
+        
+        for rowidx in range(len(lines)):
+            # print("Row index:", rowidx, "Value:", lines[rowidx][colidx-1])
+            
+            # Print the type of the character
+            # print("Type of character:", type(lines[rowidx][colidx-1]))
+
+            if lines[rowidx][colidx-1] != " " and lines[rowidx][colidx-1] != "\n":
+                if lines[rowidx][colidx-1].isdigit():
+                    numbers.append(int(lines[rowidx][colidx-1]))
+
+            if lines[rowidx][colidx-1] == "*" or lines[rowidx][colidx-1] == "+":
+                # print("Found operator: ", lines[rowidx][colidx-1])
+                operation_flag = True
+                operator = lines[rowidx][colidx-1]
+                # print("Numbers collected so far for column", colidx, "are:", combined_number_list)
+
+            
+
+        # if numbers list is not empty, combine the digits to form a number
+        if numbers:
+            combined_number = 0
+            for digit in numbers:
+                combined_number = combined_number * 10 + digit
+            # print("Combined number for column", colidx, "is:", combined_number)
+            combined_number_list.append(combined_number)
+            # print("Combined number list for column", colidx, "is:", combined_number_list)
+
+        if operation_flag:
+            # print("Performing operation:", operator, "on combined number list:", combined_number_list)
+            if operator == "+":
+                result = sum(combined_number_list)
+                results.append(result)
+            elif operator == "*":
+                result = 1
+                for num in combined_number_list:
+                    result *= num
+                # print("Result of multiplication:", result)
+                results.append(result)
+            # Reset for next operation
+            combined_number_list = []
+            operation_flag = False
+            operator = ""
 
 
-    # Last line contains the operators
-    operators = lines[-1]
-    data_lines = lines[:-1]
+    print("Final results after all operations:", results)
+    print("Final results sum:", sum(results))
 
-    # Get rid of white spaces in operators
-    operators = operators.replace(" ", "")
-    # Split operators into a list
-    operators = list(operators)
+        
 
-    # convert data_lines into list of integers
-    data_lines = [[int(value) for value in line.split()] for line in data_lines]
 
-    print(len(data_lines[0] ), "data lines read.")
-    print("Total lines including operators line:", len(operators))
-    print("Operators:", operators)
-    print("Data lines:", data_lines)
 
-    # Each column in data_lines needs to be processed with the corresponding operator
-    sum = 0
-    for col_index in range(len(data_lines[0])):
-        column_values = [line[col_index] for line in data_lines]
-        print(f"Column {col_index} values:", column_values)
 
-        operator = operators[col_index]
-
-        if operator == "*":
-            # Multiply all values in the column
-            result = 1
-            for value in column_values:
-                result *= value
-            print("result multi: ", result)
-            sum += result
-        elif operator == "+":
-            result = 0
-            # Sum the integer values in the column
-            for value in column_values:
-                result += value
-            print("result add: ", result)
-            sum += result
-
-    print("Final sum after applying all operators:", sum)
